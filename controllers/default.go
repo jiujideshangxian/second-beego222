e90007cb0e72c35a255ef7c2829bffb6d3f139b3
jiujideshangxian <1529124973@qq.com>
2020-09-23 20:29:39 +0800

    更改了

--- controllers/default.go
+++ controllers/default.go
@@ -1,14 +1,33 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
 type MainController struct {
-	beego.Controller
+	beego.Controller//匿名字段
 }
 
 func (c *MainController) Get() {
+	c.Data["Website"] = "原神"
+	c.Data["Email"] = "yuanshen.com"
+	c.TplName = "index.tpl"
+}
+
+func (c *MainController) Post() {
+	fmt.Println("post服务器")
+	user:=c.Ctx.Request.FormValue("user")
+	fmt.Println("用户名为",user)
+	psd:=c.Ctx.Request.FormValue("psd")
+	fmt.Println("密码为",psd)
+
+	if user!="yuanshen"||psd!="123456" {
+		c.Ctx.ResponseWriter.Write([]byte("对不起，数据不值钱"))
+		return
+	}
+	c.Ctx.ResponseWriter.Write([]byte("数据值钱"))
+
 	c.Data["Website"] = "原神"
 	c.Data["Email"] = "yuanshen.com"
 	c.TplName = "index.tpl"
